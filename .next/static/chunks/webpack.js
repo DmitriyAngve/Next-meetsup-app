93f92afac8fcf0d47502289aeb7bf8c368d56b54
Dmitriy Angve <kirirch129@gmail.com>
2023-04-25 13:08:46 +0300

    #

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "22132a1d4e913268"; }
+/******/ 		__webpack_require__.h = function() { return "7cf9c12b0af5668f"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
